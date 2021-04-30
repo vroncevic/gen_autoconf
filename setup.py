f875278ceadbbe8563ea86fcffbfa436112ab05a
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-05-01 00:18:15 +0200

    [gen_autoconf] updated install for setup and pip, updated version and docs

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ __author__ = 'Vladimir Roncevic'
 __copyright__ = 'Copyright 2020, https://vroncevic.github.io/gen_autoconf'
 __credits__ = ['Vladimir Roncevic']
 __license__ = 'https://github.com/vroncevic/gen_autoconf/blob/dev/LICENSE'
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 __maintainer__ = 'Vladimir Roncevic'
 __email__ = 'elektron.ronca@gmail.com'
 __status__ = 'Updated'
@@ -75,8 +75,6 @@ def install_directory():
 
 INSTALL_DIR = install_directory()
 TOOL_DIR = 'gen_autoconf/'
-CONF_DIR = '{0}{1}'.format(TOOL_DIR, 'conf/')
-TEMPLATE_DIR = '{0}{1}'.format(CONF_DIR, 'template/')
 if not bool(INSTALL_DIR):
     print('[setup] force exit from install process')
     sys.exit(127)
@@ -102,7 +100,7 @@ APPROVED_LICENSES = [
 PYP_CLASSIFIERS = SUPPORTED_PY_VERSIONS + APPROVED_LICENSES
 setup(
     name='gen_autoconf',
-    version='2.0.3',
+    version='2.0.4',
     description='Generating C project',
     author='Vladimir Roncevic',
     author_email='elektron.ronca@gmail.com',
@@ -115,60 +113,25 @@ setup(
     classifiers=PYP_CLASSIFIERS,
     packages=['gen_autoconf', 'gen_autoconf.pro'],
     install_requires=['ats-utilities'],
+    package_data = {
+        'gen_autoconf': [
+            'conf/gen_autoconf.cfg',
+            'conf/gen_autoconf_util.cfg',
+            'conf/project.yaml',
+            'conf/template/README.template',
+            'conf/template/Makefile.template',
+            'conf/template/autogen.template',
+            'conf/template/configure.template',
+            'conf/template/src/Makefile.template',
+            'conf/template/src/main.template',
+            'log/gen_autoconf.log'
+        ]
+    },
     data_files=[
         (
             '/usr/local/bin/', [
                 '{0}{1}'.format(TOOL_DIR, 'run/gen_autoconf_run.py')
             ]
-        ),
-        (
-            '{0}{1}'.format(INSTALL_DIR, CONF_DIR), [
-                '{0}{1}'.format(CONF_DIR, 'gen_autoconf.cfg')
-            ]
-        ),
-        (
-            '{0}{1}'.format(INSTALL_DIR, CONF_DIR), [
-                '{0}{1}'.format(CONF_DIR, 'gen_autoconf_util.cfg')
-            ]
-        ),
-        (
-            '{0}{1}'.format(INSTALL_DIR, CONF_DIR), [
-                '{0}{1}'.format(CONF_DIR, 'project.yaml')
-            ]
-        ),
-        (
-            '{0}{1}'.format(INSTALL_DIR, TEMPLATE_DIR), [
-                '{0}{1}'.format(TEMPLATE_DIR, 'README.template')
-            ]
-        ),
-        (
-            '{0}{1}'.format(INSTALL_DIR, TEMPLATE_DIR), [
-                '{0}{1}'.format(TEMPLATE_DIR, 'Makefile.template')
-            ]
-        ),
-        (
-            '{0}{1}'.format(INSTALL_DIR, TEMPLATE_DIR), [
-                '{0}{1}'.format(TEMPLATE_DIR, 'autogen.template')
-            ]
-        ),
-        (
-            '{0}{1}'.format(INSTALL_DIR, TEMPLATE_DIR), [
-                '{0}{1}'.format(TEMPLATE_DIR, 'configure.template')
-            ]
-        ),
-        (
-            '{0}{1}{2}'.format(INSTALL_DIR, TEMPLATE_DIR, 'src'), [
-                '{0}{1}'.format(TEMPLATE_DIR, 'src/Makefile.template')
-            ]
-        ),
-        (
-            '{0}{1}{2}'.format(INSTALL_DIR, TEMPLATE_DIR, 'src'), [
-                '{0}{1}'.format(TEMPLATE_DIR, 'src/main.template')
-            ]
-        ),
-        (
-            '{0}{1}{2}'.format(INSTALL_DIR, TOOL_DIR, 'log/'),
-            ['{0}{1}'.format(TOOL_DIR, 'log/gen_autoconf.log')]
         )
     ]
 )
