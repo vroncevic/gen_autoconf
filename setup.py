8380a06ad2af8be164d93aac8122bdd9e5da804a
Vladimir Roncevic <elektron.ronca@gmail.com>
2024-06-18 20:02:59 +0200

    [gen_autoconf] Updated option parser, code coverage, docs

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ __author__ = 'Vladimir Roncevic'
 __copyright__ = '(C) 2024, https://vroncevic.github.io/gen_autoconf'
 __credits__: List[str] = ['Vladimir Roncevic', 'Python Software Foundation']
 __license__ = 'https://github.com/vroncevic/gen_autoconf/blob/dev/LICENSE'
-__version__ = '2.7.1'
+__version__ = '2.7.2'
 __maintainer__ = 'Vladimir Roncevic'
 __email__ = 'elektron.ronca@gmail.com'
 __status__ = 'Updated'
@@ -61,7 +61,7 @@ APPROVED_LICENSES: List[str] = [
 PYP_CLASSIFIERS: List[str] = SUPPORTED_PY_VERSIONS + APPROVED_LICENSES
 setup(
     name='gen_autoconf',
-    version='2.7.1',
+    version='2.7.2',
     description='Generating C project',
     author='Vladimir Roncevic',
     author_email='elektron.ronca@gmail.com',
@@ -76,6 +76,7 @@ setup(
     install_requires=['ats-utilities'],
     package_data={
         'gen_autoconf': [
+            'py.typed',
             f'{CONF}/gen_autoconf.logo',
             f'{CONF}/gen_autoconf.cfg',
             f'{CONF}/gen_autoconf_util.cfg',
